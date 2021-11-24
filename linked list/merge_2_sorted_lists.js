4b759ad368741f02b64ade9c88bbc17c61de6978
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-11-23 19:34:11 -0600

    Merge two sorted linked lists

---
+++ linked list/merge_2_sorted_lists.js
@@ -0,0 +1,43 @@
+/**
+ * 21. Merge Two Sorted Lists
+ * Merge two sorted linked lists and return it as a sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ * Input: l1 = [1,2,4], l2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ * 
+function ListNode(val, next) {
+  this.val = val === undefined ? 0 : val;
+  this.next = next === undefined ? null : next;
+}
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+var mergeTwoLists = function (l1, l2) {
+  let mergedHead = { val: -1, next: null };
+  let crt = mergedHead;
+  while (l1 && l2) {
+    if (l1.val > l2.val) {
+      crt.next = l2;
+      l2 = l2.next;
+    } else {
+      crt.next = l1;
+      l1 = l1.next;
+    }
+    crt = crt.next;
+  }
+  crt.next = l1 || l2;
+
+  return mergedHead.next;
+};
