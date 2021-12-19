9d72e37b4463d2048358935b5c27ae19ab39ec9b
Bogdan Plavin <bohdan.plavin@gmail.com>
2021-12-18 19:54:42 -0600

    383. Ransom Note

--- string/ransom_note.js
+++ string/ransom_note.js
@@ -1,3 +1,13 @@
+// Given two stings ransomNote and magazine, return true if ransomNote can be constructed from magazine and false otherwise.
+
+// Each letter in magazine can only be used once in ransomNote.
+
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+
 var canConstruct = function (ransomNote, magazine) {
   ransomNote.split("").sort().join("");
   magazine.split("").sort().join("");
