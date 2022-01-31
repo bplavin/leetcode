2bec9d3f50cd51616f963e3d62cd01c6e058fc9e
Bogdan Plavin <bohdan.plavin@gmail.com>
2022-01-30 21:35:49 -0600

    Min value of BST

---
+++ tree/Min value of BST.js
@@ -0,0 +1,26 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+let min = 99999999999999;
+
+let dfs = (node) => {
+  if (node == null) return;
+  min = Math.min(node.val, min);
+  dfs(node.left, min);
+  dfs(node.right, min);
+
+  return min;
+};
+
+let root = new TreeNode(5);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(1);
+
+console.log(dfs(root));
